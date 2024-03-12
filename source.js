746ea5c577c96848a9ad02b0165623c6da28d785
sb69 <126227400+sb1969vb@users.noreply.github.com>
2024-03-12 08:05:26 +0200

    Update source.js

--- source.js
+++ source.js
@@ -144,8 +144,8 @@
         "title": title,
         "original_title": original_title,
         "overview": elem.description || elem.shortDescription || '',
-        "img": kp_prox + elem.posterUrlPreview ||kp_prox + elem.posterUrl || ' ',
-        "background_image": kp_prox + elem.coverUrl || kp_prox + elem.posterUrl || kp_prox + elem.posterUrlPreview || '',
+        "img": 'https://corseu.sb69.workers.dev/' + elem.posterUrlPreview ||'https://corseu.sb69.workers.dev/' + elem.posterUrl || ' ',
+        "background_image": 'https://corseu.sb69.workers.dev/' + elem.coverUrl || 'https://corseu.sb69.workers.dev/' + elem.posterUrl || 'https://corseu.sb69.workers.dev/' + elem.posterUrlPreview || '',
         "genres": elem.genres && elem.genres.map(function (e) {
           if (e.genre === 'для взрослых') {
             adult = true;
